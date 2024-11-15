3b9c70f19f1f1699c16e9dee2fb91d5ba196b857
Reza Ameli <rameli@rameli.dev>
2024-11-14 19:23:36 -0500

    wip

--- lib/mat-transpose/mat-transpose.h
+++ lib/mat-transpose/mat-transpose.h
@@ -4,8 +4,9 @@
 
 void TransposeNaive(uint64_t* src, uint64_t* dst, uint32_t rowCount, uint32_t colCount);
 void TransposeNaiveInPlace(uint64_t* matrix, uint32_t rowCount);
-void TransposeTiledMultiThreaded(uint64_t* src, uint64_t* dst, uint32_t rowCount, uint32_t colCount, uint32_t tileSize, uint32_t numThreads);
 
+void TransposeTiledMultiThreaded(uint64_t* src, uint64_t* dst, uint32_t rowCount, uint32_t colCount, uint32_t tileSize, uint32_t numThreads);
+void TransposeTiledMultiThreadedOptimized(uint64_t* src, uint64_t* dst, uint32_t rowCount, uint32_t colCount, uint32_t tileSize, uint32_t numThreads);
 void TransposeTiledInPlaceMultiThreaded(uint64_t* matrix, uint32_t rowCount, uint32_t tileSize, uint32_t numThreads);
 
 void TransposeRecursive(uint64_t* src, uint64_t* dst, uint32_t rowCount, uint32_t colCount);
